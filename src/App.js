a412979946ca0d686a9d7f77b77fc6f25c667cd8
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-08-19 15:28:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSasdandbasdasdox</h1>
+      <h2>Start editingas to see some magic happen!</h2>
+    </div>
+  );
+}
